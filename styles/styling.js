28b4b12dc2b8b207d2d0ab4587c0154d597880a9
DannyHussain <dannyh_786@hotmail.co.uk>
2017-02-09 19:40:09 +0000

    Fixed the issue where resizing the window on desktop would misposition the elements.

--- styles/styling.js
+++ styles/styling.js
@@ -1,3 +1,4 @@
+/*----Method to create a responsive menu icon for mobile version website----*/
 function myFunction() {
     var x = document.getElementById("myNav");
     if (x.className === "navigation") {
